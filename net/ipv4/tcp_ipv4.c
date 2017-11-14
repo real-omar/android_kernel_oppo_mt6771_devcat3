254266d77bc209c2e71ed803af6bf44abdd95a2e
Stephen Hemminger <stephen@networkplumber.org>
2017-11-14 08:25:49 -0800

    tcp: Namespace-ify sysctl_tcp_default_congestion_control
    Make default TCP default congestion control to a per namespace
    value. This changes default congestion control to a pointer to congestion ops
    (rather than implicit as first element of available lsit).
    
    The congestion control setting of new namespaces is inherited
    from the current setting of the root namespace.
    
    Change-Id: Ic8b93798df8df6f60d3642392f1fcf32c41a85c8
    Signed-off-by: Stephen Hemminger <sthemmin@microsoft.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -2500,6 +2500,8 @@ static void __net_exit tcp_sk_exit(struct net *net)
 {
 	int cpu;
 
+	module_put(net->ipv4.tcp_congestion_control->owner);
+
 	for_each_possible_cpu(cpu)
 		inet_ctl_sock_destroy(*per_cpu_ptr(net->ipv4.tcp_sk, cpu));
 	free_percpu(net->ipv4.tcp_sk);
@@ -2575,6 +2577,13 @@ static int __net_init tcp_sk_init(struct net *net)
 	#ifdef OPLUS_BUG_STABILITY
 	net->ipv4.sysctl_tcp_random_timestamp = 1;
 	#endif /* OPLUS_BUG_STABILITY */
+	/* Reno is always built in */
+	if (!net_eq(net, &init_net) &&
+	    try_module_get(init_net.ipv4.tcp_congestion_control->owner))
+		net->ipv4.tcp_congestion_control = init_net.ipv4.tcp_congestion_control;
+	else
+		net->ipv4.tcp_congestion_control = &tcp_reno;
+
 	return 0;
 fail:
 	tcp_sk_exit(net);
