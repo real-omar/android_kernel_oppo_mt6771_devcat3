90da903e99ef25bb5818604cb0cf92015e87ced2
Lawrence Brakmo <brakmo@fb.com>
2018-01-25 16:14:14 -0800

    UPSTREAM: bpf: Add BPF_SOCK_OPS_RETRANS_CB
    Adds support for calling sock_ops BPF program when there is a
    retransmission. Three arguments are used; one for the sequence number,
    another for the number of segments retransmitted, and the last one for
    the return value of tcp_transmit_skb (0 => success).
    Does not include syn-ack retransmissions.
    
    New op: BPF_SOCK_OPS_RETRANS_CB.
    
    Change-Id: I62056bd70e01550df700e7569878aa3da6a7eec5
    Signed-off-by: Lawrence Brakmo <brakmo@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2992,6 +2992,10 @@ start:
 	oplus_handle_retransmit(sk, -1); // in this function, tcp_transmit_skb is called again.
 	//#endif /* OPLUS_FEATURE_DATA_EVAL */
 
+	if (BPF_SOCK_OPS_TEST_FLAG(tp, BPF_SOCK_OPS_RETRANS_CB_FLAG))
+		tcp_call_bpf_3arg(sk, BPF_SOCK_OPS_RETRANS_CB,
+				  TCP_SKB_CB(skb)->seq, segs, err);
+
 	if (likely(!err)) {
 		TCP_SKB_CB(skb)->sacked |= TCPCB_EVER_RETRANS;
 	} else if (err != -EBUSY) {
