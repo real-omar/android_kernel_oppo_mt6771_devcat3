b885a0bff0f367a9842a2b6948f75bb1c466e761
Kees Cook <keescook@chromium.org>
2018-05-08 12:55:26 -0700

    UPSTREAM: mm: Use overflow helpers in kvmalloc()
    Instead of open-coded multiplication and bounds checking, use the new
    overflow helper. Additionally prepare for vmalloc() users to add
    array_size()-family helpers in the future.
    
    Change-Id: I38470b8d03d0506a39ca6bd343d3e6771a050493
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -25,6 +25,7 @@
 #include <linux/err.h>
 #include <linux/page_ref.h>
 #include <linux/memremap.h>
+#include <linux/overflow.h>
 
 #if defined(OPLUS_FEATURE_VIRTUAL_RESERVE_MEMORY) && defined(CONFIG_VIRTUAL_RESERVE_MEMORY)
 #include <linux/sched.h>
@@ -589,10 +590,12 @@ static inline void *kvzalloc(size_t size, gfp_t flags)
 
 static inline void *kvmalloc_array(size_t n, size_t size, gfp_t flags)
 {
-	if (size != 0 && n > SIZE_MAX / size)
+	size_t bytes;
+
+	if (unlikely(check_mul_overflow(n, size, &bytes)))
 		return NULL;
 
-	return kvmalloc(n * size, flags);
+	return kvmalloc(bytes, flags);
 }
 
 static inline void *kvcalloc(size_t n, size_t size, gfp_t flags)
