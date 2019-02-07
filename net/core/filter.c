4acfed2d7088fd0e097dc5bffb244cc65f66512c
Willem de Bruijn <willemb@google.com>
2019-02-07 14:54:16 -0500

    UPSTREAM: bpf: only adjust gso_size on bytestream protocols
    [ Upstream commit b90efd2258749e04e1b3f71ef0d716f2ac2337e0 ]
    
    bpf_skb_change_proto and bpf_skb_adjust_room change skb header length.
    For GSO packets they adjust gso_size to maintain the same MTU.
    
    The gso size can only be safely adjusted on bytestream protocols.
    Commit d02f51cbcf12 ("bpf: fix bpf_skb_adjust_net/bpf_skb_proto_xlat
    to deal with gso sctp skbs") excluded SKB_GSO_SCTP.
    
    Since then type SKB_GSO_UDP_L4 has been added, whose contents are one
    gso_size unit per datagram. Also exclude these.
    
    Move from a blacklist to a whitelist check to future proof against
    additional such new GSO types, e.g., for fraglist based GRO.
    
    Fixes: bec1f6f69736 ("udp: generate gso with UDP_SEGMENT")
    Change-Id: Ie0ab55542222d64de2425e84650c106cfe2ebca8
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- net/core/filter.c
+++ net/core/filter.c
@@ -2621,6 +2621,9 @@ static int bpf_skb_proto_4_to_6(struct sk_buff *skb)
 	u32 off = skb_mac_header_len(skb);
 	int ret;
 
+	if (!skb_is_gso_tcp(skb))
+		return -ENOTSUPP;
+
 	ret = skb_cow(skb, len_diff);
 	if (unlikely(ret < 0))
 		return ret;
@@ -2657,6 +2660,12 @@ static int bpf_skb_proto_6_to_4(struct sk_buff *skb)
 	u32 off = skb_mac_header_len(skb);
 	int ret;
 
+<<<<<<< HEAD
+=======
+	if (!skb_is_gso_tcp(skb))
+		return -ENOTSUPP;
+
+>>>>>>> 105d6ee3d07d (UPSTREAM: bpf: only adjust gso_size on bytestream protocols)
 	ret = skb_unclone(skb, GFP_ATOMIC);
 	if (unlikely(ret < 0))
 		return ret;
@@ -2777,6 +2786,12 @@ static int bpf_skb_net_grow(struct sk_buff *skb, u32 len_diff)
 	u32 off = skb_mac_header_len(skb) + bpf_skb_net_base_len(skb);
 	int ret;
 
+<<<<<<< HEAD
+=======
+	if (!skb_is_gso_tcp(skb))
+		return -ENOTSUPP;
+
+>>>>>>> 105d6ee3d07d (UPSTREAM: bpf: only adjust gso_size on bytestream protocols)
 	ret = skb_cow(skb, len_diff);
 	if (unlikely(ret < 0))
 		return ret;
@@ -2801,6 +2816,12 @@ static int bpf_skb_net_shrink(struct sk_buff *skb, u32 len_diff)
 	u32 off = skb_mac_header_len(skb) + bpf_skb_net_base_len(skb);
 	int ret;
 
+<<<<<<< HEAD
+=======
+	if (!skb_is_gso_tcp(skb))
+		return -ENOTSUPP;
+
+>>>>>>> 105d6ee3d07d (UPSTREAM: bpf: only adjust gso_size on bytestream protocols)
 	ret = skb_unclone(skb, GFP_ATOMIC);
 	if (unlikely(ret < 0))
 		return ret;
