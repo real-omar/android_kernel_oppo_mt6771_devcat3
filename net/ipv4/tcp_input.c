3ef2a55e53bf58e1e16ea5b613da22801083635b
Stanislav Fomichev <sdf@google.com>
2019-07-02 09:13:56 -0700

    BACKPORT: bpf: add BPF_CGROUP_SOCK_OPS callback that is executed on every RTT
    Performance impact should be minimal because it's under a new
    BPF_SOCK_OPS_RTT_CB_FLAG flag that has to be explicitly enabled.
    
    Suggested-by: Eric Dumazet <edumazet@google.com>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: Priyaranjan Jha <priyarjha@google.com>
    Cc: Yuchung Cheng <ycheng@google.com>
    Cc: Soheil Hassas Yeganeh <soheil@google.com>
    Acked-by: Soheil Hassas Yeganeh <soheil@google.com>
    Acked-by: Yuchung Cheng <ycheng@google.com>
    Change-Id: I19814edf78101f87e6dc364343f8173d9e230850
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -801,6 +801,8 @@ static void tcp_rtt_estimator(struct sock *sk, long mrtt_us)
 				tp->rttvar_us -= (tp->rttvar_us - tp->mdev_max_us) >> 2;
 			tp->rtt_seq = tp->snd_nxt;
 			tp->mdev_max_us = tcp_rto_min_us(sk);
+
+			tcp_bpf_rtt(sk);
 		}
                 //#ifdef OPLUS_FEATURE_WIFI_SLA
                 //Add code for appo sla function
@@ -815,6 +817,8 @@ static void tcp_rtt_estimator(struct sock *sk, long mrtt_us)
 		tp->rttvar_us = max(tp->mdev_us, tcp_rto_min_us(sk));
 		tp->mdev_max_us = tp->rttvar_us;
 		tp->rtt_seq = tp->snd_nxt;
+
+		tcp_bpf_rtt(sk);
 	}
 	tp->srtt_us = max(1U, srtt);
 }
