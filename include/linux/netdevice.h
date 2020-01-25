e3d4efa2d984788a2202baafa6c3f71bd9921236
Jiri Pirko <jiri@mellanox.com>
2020-01-25 12:17:08 +0100

    BACKPORT: net: introduce dev_net notifier register/unregister variants
    Introduce dev_net variants of netdev notifier register/unregister functions
    and allow per-net notifier to follow the netdevice into the namespace it is
    moved to.
    
    Change-Id: I23d908568bf3acfb7ccc516acf30fb68285cb132
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -855,6 +855,11 @@ struct xfrmdev_ops {
 };
 #endif
 
+struct netdev_net_notifier {
+	struct list_head list;
+	struct notifier_block *nb;
+};
+
 /*
  * This structure defines the management hooks for network devices.
  * The following hooks can be defined; unless noted otherwise, they are
@@ -1662,6 +1667,10 @@ enum netdev_priv_flags {
  *			switch driver and used to set the phys state of the
  *			switch port.
  *
+ *	@net_notifier_list:	List of per-net netdev notifier block
+ *				that follow this device when it is moved
+ *				to another network namespace.
+ *
  *	FIXME: cleanup struct net_device such that network protocol info
  *	moves out.
  */
@@ -1941,6 +1950,8 @@ struct net_device {
 	struct lock_class_key	*qdisc_tx_busylock;
 	struct lock_class_key	*qdisc_running_key;
 	bool			proto_down;
+
+	struct list_head	net_notifier_list;
 };
 #define to_net_dev(d) container_of(d, struct net_device, dev)
 
@@ -2352,6 +2363,12 @@ int unregister_netdevice_notifier(struct notifier_block *nb);
 int register_netdevice_notifier_net(struct net *net, struct notifier_block *nb);
 int unregister_netdevice_notifier_net(struct net *net,
 				      struct notifier_block *nb);
+int register_netdevice_notifier_dev_net(struct net_device *dev,
+					struct notifier_block *nb,
+					struct netdev_net_notifier *nn);
+int unregister_netdevice_notifier_dev_net(struct net_device *dev,
+					  struct notifier_block *nb,
+					  struct netdev_net_notifier *nn);
 
 struct netdev_notifier_info {
 	struct net_device *dev;
