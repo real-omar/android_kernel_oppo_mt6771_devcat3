e844b77d3f920b280fcdb4f322ea141c7bdf5064
Macpaul Lin <macpaul.lin@mediatek.com>
2020-06-24 16:59:47 +0300

    usb: host: xhci-mtk: avoid runtime suspend when removing hcd
    commit a24d5072e87457a14023ee1dd3fc8b1e76f899ef upstream.
    
    When runtime suspend was enabled, runtime suspend might happen
    when xhci is removing hcd. This might cause kernel panic when hcd
    has been freed but runtime pm suspend related handle need to
    reference it.
    
    Signed-off-by: Macpaul Lin <macpaul.lin@mediatek.com>
    Reviewed-by: Chunfeng Yun <chunfeng.yun@mediatek.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Link: https://lore.kernel.org/r/20200624135949.22611-4-mathias.nyman@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,79 +4,28 @@ echo "========================================="
 echo "Complete Kernel and Modules Build Script"
 echo "========================================="
 
-# ============================================
-# CONFIGURATION — Edit paths here
-# ============================================
-
-# Root of your kernel-dev workspace
-KERNEL_DEV_ROOT="/home/devcat3/Desktop/oppo/"
-
-# Kernel source directory (relative to KERNEL_DEV_ROOT)
-KERNEL_SOURCE_DIR="android_kernel_oppo_mt6771"
-
-# Full path to kernel source (derived)
-KERNEL_DIR="${KERNEL_DEV_ROOT}/${KERNEL_SOURCE_DIR}"
-
-# Toolchain paths (relative to KERNEL_DIR)
-GCC_AARCH64_REL="toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
-GCC_ARM32_REL="toolchain/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
-CLANG_REL="toolchain/clang/host/linux-x86/clang-r383902/bin"
-
-# Vendor modules root
-VENDOR_MODULES="${KERNEL_DEV_ROOT}/vendor/mediatek/kernel_modules"
-
-# Where collected .ko files go (flat directory)
-MODULES_COLLECTION="${KERNEL_DEV_ROOT}/modules_collection"
-
-# Defconfig name
-DEFCONFIG="oppo6771_defconfig"
-
-# Parallel build jobs
-BUILD_JOBS=4
-
-# Target architecture
-BUILD_ARCH="arm64"
-
-# Clang triple
-CLANG_TRIPLE="aarch64-linux-gnu-"
-
-# ============================================
-# DERIVED PATHS — Do not edit below this line
-# ============================================
-KERNEL_OUT="${KERNEL_DIR}/out"
-AUTOCONF_H="${KERNEL_OUT}/include/generated/autoconf.h"
-CROSS_COMPILE="${KERNEL_DIR}/${GCC_AARCH64_REL}"
-CROSS_COMPILE_ARM32="${KERNEL_DIR}/${GCC_ARM32_REL}"
-CLANG_TOOL_PATH="${KERNEL_DIR}/${CLANG_REL}"
-
-# Connectivity module shortcuts (derived from VENDOR_MODULES)
-CONN_DIR="${VENDOR_MODULES}/connectivity"
-CONN_COMMON="${CONN_DIR}/common"
-CONN_WLAN_ADAPTOR="${CONN_DIR}/wlan/adaptor"
-CONN_WLAN_GEN3="${CONN_DIR}/wlan/core/gen3"
-CONN_WLAN_GEN2="${CONN_DIR}/wlan/core/gen2"
-CONN_WLAN_GEN4_7668="${CONN_DIR}/wlan/core/gen4-mt7668"
-CONN_WLAN_GEN4_7663="${CONN_DIR}/wlan/core/gen4-mt7663"
-CONN_WLAN_GEN4M="${CONN_DIR}/wlan/core/gen4m"
-CONN_BT="${CONN_DIR}/bt/mt66xx/legacy"
-CONN_FM="${CONN_DIR}/fmradio"
-CONN_GPS="${CONN_DIR}/gps"
+# Navigate to kernel directory
+cd /home/omar/Desktop/kernel-dev/android_kernel_oppo_mt6771-oppo-mt6771_r_11 || exit 1
 
 # ============================================
 # PART 1: SET ENVIRONMENT VARIABLES
 # ============================================
 echo ""
 echo "Step 1: Setting up environment..."
+export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=$(pwd)/toolchain/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export ARCH=arm64
+export CLANG_TOOL_PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/
+export PATH=${CLANG_TOOL_PATH}:${PATH//"${CLANG_TOOL_PATH}:"}
 
-cd "${KERNEL_DIR}" || exit 1
+# Set paths
+KERNEL_OUT=$(pwd)/out
+AUTOCONF_H=${KERNEL_OUT}/include/generated/autoconf.h
+VENDOR_MODULES=/home/omar/Desktop/kernel-dev/vendor/mediatek/kernel_modules
+MODULES_COLLECTION=/home/omar/Desktop/kernel-dev/modules_collection
 
-export CROSS_COMPILE="${CROSS_COMPILE}"
-export CROSS_COMPILE_ARM32="${CROSS_COMPILE_ARM32}"
-export ARCH="${BUILD_ARCH}"
-export CLANG_TOOL_PATH="${CLANG_TOOL_PATH}"
-export PATH="${CLANG_TOOL_PATH}:${PATH//"${CLANG_TOOL_PATH}:"}"
-
-mkdir -p "${MODULES_COLLECTION}"
+# Create module collection directory (flat - all .ko files go here directly)
+mkdir -p ${MODULES_COLLECTION}
 
 # ============================================
 # PART 2: BUILD THE KERNEL
@@ -85,27 +34,27 @@ echo ""
 echo "Step 2: Building kernel..."
 echo "========================================="
 
-make -C "${KERNEL_DIR}" O="${KERNEL_OUT}" \
+make -C $(pwd) O=${KERNEL_OUT} \
   CC=clang \
   LD=ld.lld \
   AS=aarch64-linux-android-as \
-  ARCH="${BUILD_ARCH}" \
-  CLANG_TRIPLE="${CLANG_TRIPLE}" \
-  "${DEFCONFIG}"
+  ARCH=arm64 \
+  CLANG_TRIPLE=aarch64-linux-gnu- \
+  oppo6771_defconfig
 
-make -C "${KERNEL_DIR}" O="${KERNEL_OUT}" \
+make -C $(pwd) O=${KERNEL_OUT} \
   CC=clang \
   LD=ld.lld \
   AS=aarch64-linux-android-as \
-  ARCH="${BUILD_ARCH}" \
-  CLANG_TRIPLE="${CLANG_TRIPLE}" \
+  ARCH=arm64 \
+  CLANG_TRIPLE=aarch64-linux-gnu- \
   KCFLAGS="-Wno-strict-prototypes" \
-  -j"${BUILD_JOBS}"
+  -j4
 
 # Copy kernel images
-cp "${KERNEL_OUT}/arch/${BUILD_ARCH}/boot/Image"        "${KERNEL_DIR}/arch/${BUILD_ARCH}/boot/Image"
-cp "${KERNEL_OUT}/arch/${BUILD_ARCH}/boot/Image.gz"     "${KERNEL_DIR}/arch/${BUILD_ARCH}/boot/Image.gz"
-cp "${KERNEL_OUT}/arch/${BUILD_ARCH}/boot/Image.gz-dtb" "${KERNEL_DIR}/arch/${BUILD_ARCH}/boot/Image.gz-dtb"
+cp ${KERNEL_OUT}/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+cp ${KERNEL_OUT}/arch/arm64/boot/Image.gz $(pwd)/arch/arm64/boot/Image.gz
+cp ${KERNEL_OUT}/arch/arm64/boot/Image.gz-dtb $(pwd)/arch/arm64/boot/Image.gz-dtb
 
 echo "✓ Kernel built successfully!"
 
@@ -132,8 +81,10 @@ merge_symvers() {
         return 1
     fi
 
+    # Create dst symvers if it doesn't exist yet
     touch "$dst"
 
+    # Append only lines not already present (avoid duplicate symbols)
     local added=0
     while IFS= read -r line; do
         if ! grep -qF "$line" "$dst"; then
@@ -157,22 +108,23 @@ build_module() {
         echo "Building: $name"
         cd "$dir"
 
-        make -C "${KERNEL_OUT}" M="$(pwd)" modules \
-            AUTOCONF_H="${AUTOCONF_H}" \
+        make -C ${KERNEL_OUT} M=$(pwd) modules \
+            AUTOCONF_H=${AUTOCONF_H} \
             CC=clang \
             LD=ld.lld \
-            ARCH="${BUILD_ARCH}" \
-            CLANG_TRIPLE="${CLANG_TRIPLE}" \
-            CROSS_COMPILE="${CROSS_COMPILE}" \
-            CROSS_COMPILE_ARM32="${CROSS_COMPILE_ARM32}"
+            ARCH=arm64 \
+            CLANG_TRIPLE=aarch64-linux-gnu- \
+            CROSS_COMPILE=${CROSS_COMPILE} \
+            CROSS_COMPILE_ARM32=${CROSS_COMPILE_ARM32}
 
         if [ $? -eq 0 ]; then
             echo "✓ $name built successfully"
-            cd "${KERNEL_DIR}"
+            # Always return to kernel dir so relative paths stay stable
+            cd /home/omar/Desktop/kernel-dev/android_kernel_oppo_mt6771-oppo-mt6771_r_11
             return 0
         else
             echo "✗ $name failed to build"
-            cd "${KERNEL_DIR}"
+            cd /home/omar/Desktop/kernel-dev/android_kernel_oppo_mt6771-oppo-mt6771_r_11
             return 1
         fi
     else
@@ -195,50 +147,65 @@ build_module() {
 # ============================================
 
 # --- 1. Common ---
-build_module "${CONN_COMMON}" "Common Modules"
+# Needs only kernel symbols (already in KERNEL_OUT/Module.symvers)
+build_module "${VENDOR_MODULES}/connectivity/common" "Common Modules"
 
 # --- 2. WLAN Adaptor ---
-merge_symvers "${CONN_COMMON}" "${CONN_WLAN_ADAPTOR}"
-build_module  "${CONN_WLAN_ADAPTOR}" "WLAN Adaptor"
+# Depends on: kernel + common
+# Merge common's symvers in before building
+merge_symvers "${VENDOR_MODULES}/connectivity/common" \
+              "${VENDOR_MODULES}/connectivity/wlan/adaptor"
+build_module "${VENDOR_MODULES}/connectivity/wlan/adaptor" "WLAN Adaptor"
 
 # --- 3. Wi-Fi Gen3 ---
-merge_symvers "${CONN_COMMON}"       "${CONN_WLAN_GEN3}"
-merge_symvers "${CONN_WLAN_ADAPTOR}" "${CONN_WLAN_GEN3}"
-build_module  "${CONN_WLAN_GEN3}" "Wi-Fi Gen3"
+# Depends on: kernel + common + wlan/adaptor
+#   (needs register_set_p2p_mode_handler from adaptor)
+merge_symvers "${VENDOR_MODULES}/connectivity/common" \
+              "${VENDOR_MODULES}/connectivity/wlan/core/gen3"
+merge_symvers "${VENDOR_MODULES}/connectivity/wlan/adaptor" \
+              "${VENDOR_MODULES}/connectivity/wlan/core/gen3"
+build_module "${VENDOR_MODULES}/connectivity/wlan/core/gen3" "Wi-Fi Gen3"
 
 # Uncomment below if you ever re-enable gen2/gen4 variants:
-#merge_symvers "${CONN_COMMON}"       "${CONN_WLAN_GEN2}"
-#merge_symvers "${CONN_WLAN_ADAPTOR}" "${CONN_WLAN_GEN2}"
-#build_module  "${CONN_WLAN_GEN2}" "Wi-Fi Gen2"
+#merge_symvers "${VENDOR_MODULES}/connectivity/common"        "${VENDOR_MODULES}/connectivity/wlan/core/gen2"
+#merge_symvers "${VENDOR_MODULES}/connectivity/wlan/adaptor"  "${VENDOR_MODULES}/connectivity/wlan/core/gen2"
+#build_module  "${VENDOR_MODULES}/connectivity/wlan/core/gen2" "Wi-Fi Gen2"
 
-#merge_symvers "${CONN_COMMON}"       "${CONN_WLAN_GEN4_7668}"
-#merge_symvers "${CONN_WLAN_ADAPTOR}" "${CONN_WLAN_GEN4_7668}"
-#build_module  "${CONN_WLAN_GEN4_7668}" "Wi-Fi Gen4 MT7668"
+#merge_symvers "${VENDOR_MODULES}/connectivity/common"              "${VENDOR_MODULES}/connectivity/wlan/core/gen4-mt7668"
+#merge_symvers "${VENDOR_MODULES}/connectivity/wlan/adaptor"        "${VENDOR_MODULES}/connectivity/wlan/core/gen4-mt7668"
+#build_module  "${VENDOR_MODULES}/connectivity/wlan/core/gen4-mt7668" "Wi-Fi Gen4 MT7668"
 
-#merge_symvers "${CONN_COMMON}"       "${CONN_WLAN_GEN4_7663}"
-#merge_symvers "${CONN_WLAN_ADAPTOR}" "${CONN_WLAN_GEN4_7663}"
-#build_module  "${CONN_WLAN_GEN4_7663}" "Wi-Fi Gen4 MT7663"
+#merge_symvers "${VENDOR_MODULES}/connectivity/common"              "${VENDOR_MODULES}/connectivity/wlan/core/gen4-mt7663"
+#merge_symvers "${VENDOR_MODULES}/connectivity/wlan/adaptor"        "${VENDOR_MODULES}/connectivity/wlan/core/gen4-mt7663"
+#build_module  "${VENDOR_MODULES}/connectivity/wlan/core/gen4-mt7663" "Wi-Fi Gen4 MT7663"
 
-#merge_symvers "${CONN_COMMON}"       "${CONN_WLAN_GEN4M}"
-#merge_symvers "${CONN_WLAN_ADAPTOR}" "${CONN_WLAN_GEN4M}"
-#build_module  "${CONN_WLAN_GEN4M}" "Wi-Fi Gen4m"
+#merge_symvers "${VENDOR_MODULES}/connectivity/common"         "${VENDOR_MODULES}/connectivity/wlan/core/gen4m"
+#merge_symvers "${VENDOR_MODULES}/connectivity/wlan/adaptor"   "${VENDOR_MODULES}/connectivity/wlan/core/gen4m"
+#build_module  "${VENDOR_MODULES}/connectivity/wlan/core/gen4m" "Wi-Fi Gen4m"
 
 # --- 4. Bluetooth ---
-merge_symvers "${CONN_COMMON}" "${CONN_BT}"
-build_module  "${CONN_BT}" "Bluetooth"
+# Depends on: kernel + common
+#   (needs mtk_wcn_wmt_* and mtk_wcn_stp_* from common/wmt)
+merge_symvers "${VENDOR_MODULES}/connectivity/common" \
+              "${VENDOR_MODULES}/connectivity/bt/mt66xx/legacy"
+build_module "${VENDOR_MODULES}/connectivity/bt/mt66xx/legacy" "Bluetooth"
 
 # --- 5. FM Radio ---
-merge_symvers "${CONN_COMMON}" "${CONN_FM}"
-build_module  "${CONN_FM}" "FM Radio"
+# Depends on: kernel + common
+merge_symvers "${VENDOR_MODULES}/connectivity/common" \
+              "${VENDOR_MODULES}/connectivity/fmradio"
+build_module "${VENDOR_MODULES}/connectivity/fmradio" "FM Radio"
 
 # --- 6. GPS ---
-merge_symvers "${CONN_COMMON}" "${CONN_GPS}"
-build_module  "${CONN_GPS}" "GPS"
+# Depends on: kernel + common
+merge_symvers "${VENDOR_MODULES}/connectivity/common" \
+              "${VENDOR_MODULES}/connectivity/gps"
+build_module "${VENDOR_MODULES}/connectivity/gps" "GPS"
 
-# --- 7. Remaining standalone modules ---
-build_module "${VENDOR_MODULES}/met_drv/4.14" "met_drv"
-build_module "${VENDOR_MODULES}/met_drv_v2"   "met_drv_v2"
-build_module "${VENDOR_MODULES}/udc"           "udc"
+# --- 7. Remaining standalone modules (no connectivity deps) ---
+#build_module "${VENDOR_MODULES}/met_drv/4.14" "met_drv"
+#build_module "${VENDOR_MODULES}/met_drv_v2"   "met_drv_v2"
+#build_module "${VENDOR_MODULES}/udc"           "udc"
 
 
 # ============================================
@@ -248,13 +215,15 @@ echo ""
 echo "Step 4: Collecting all modules..."
 echo "========================================="
 
+# Collect all .ko files from vendor modules into a single flat directory
 echo "Collecting all vendor modules..."
-find "${VENDOR_MODULES}" -name "*.ko" -type f 2>/dev/null | while read module; do
+find ${VENDOR_MODULES} -name "*.ko" -type f 2>/dev/null | while read module; do
     cp -v "$module" "${MODULES_COLLECTION}/" 2>/dev/null
 done
 
+# Also collect .ko files from the kernel out directory
 echo "Collecting kernel out modules..."
-find "${KERNEL_OUT}" -name "*.ko" -type f ! -path "*/modules_install/*" 2>/dev/null | while read module; do
+find ${KERNEL_OUT} -name "*.ko" -type f ! -path "*/modules_install/*" 2>/dev/null | while read module; do
     cp -v "$module" "${MODULES_COLLECTION}/" 2>/dev/null
 done
 
@@ -265,12 +234,62 @@ echo ""
 echo "Step 5: Creating module information file..."
 echo "========================================="
 
-INFO_FILE="${MODULES_COLLECTION}/modules_info.txt"
-cat > "${INFO_FILE}" << EOF
+INFO_FILE=${MODULES_COLLECTION}/modules_info.txt
+cat > ${INFO_FILE} << EOF
 ========================================
 Kernel and Modules Build Information
 ========================================
 Build Date: $(date)
-Kernel Source: ${KERNEL_DIR}
-Kernel Version: $(make -C "${KERNEL_OUT}" kernelrelease 2>/dev/null)
-Build Architecture: ${BUILD_ARCH}
+Kernel Source: $(pwd)
+Kernel Version: $(make -C ${KERNEL_OUT} kernelrelease 2>/dev/null)
+Build Architecture: arm64
+
+========================================
+Module Collection Summary
+========================================
+EOF
+
+echo "" >> ${INFO_FILE}
+echo "Module counts:" >> ${INFO_FILE}
+echo "  Total modules: $(find ${MODULES_COLLECTION} -maxdepth 1 -name "*.ko" -type f | wc -l)" >> ${INFO_FILE}
+echo "" >> ${INFO_FILE}
+
+echo "Detailed module information:" >> ${INFO_FILE}
+echo "========================================" >> ${INFO_FILE}
+echo "" >> ${INFO_FILE}
+
+# Add detailed info for each module
+find ${MODULES_COLLECTION} -maxdepth 1 -name "*.ko" -type f | sort | while read module; do
+    echo "Module: $(basename "$module")" >> ${INFO_FILE}
+    echo "  Location: $module" >> ${INFO_FILE}
+    modinfo "$module" 2>/dev/null | grep -E "description|author|license|depends|vermagic" | sed 's/^/  /' >> ${INFO_FILE}
+    echo "" >> ${INFO_FILE}
+done
+
+# ============================================
+# PART 6: FINAL SUMMARY
+# ============================================
+echo ""
+echo "========================================="
+echo "BUILD COMPLETE!"
+echo "========================================="
+echo ""
+echo "Kernel images:"
+echo "  - ${KERNEL_OUT}/arch/arm64/boot/Image"
+echo "  - ${KERNEL_OUT}/arch/arm64/boot/Image.gz"
+echo "  - ${KERNEL_OUT}/arch/arm64/boot/Image.gz-dtb"
+echo ""
+echo "All modules collected in:"
+echo "  ${MODULES_COLLECTION}"
+echo ""
+echo "Module summary:"
+echo "  Total: $(find ${MODULES_COLLECTION} -maxdepth 1 -name "*.ko" -type f | wc -l) modules"
+echo ""
+echo "Module information: ${INFO_FILE}"
+echo "Module loading script: ${LOAD_SCRIPT}"
+echo ""
+echo "To use these modules on your device:"
+echo "  1. Copy ${MODULES_COLLECTION} to your device"
+echo "  2. Run: sh ${MODULES_COLLECTION}/load_modules.sh"
+echo ""
+echo "========================================="
