318c0541a0ca690d68eca0946f2c16f582239234
real_omar <mero.ewida10@gmail.com>
2026-09-21 16:36:34 +0300

    fix conflict

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -1380,7 +1380,6 @@ static int loop_set_block_size(struct loop_device *lo, unsigned long arg)
 	if (arg < 512 || arg > PAGE_SIZE || !is_power_of_2(arg))
 		return -EINVAL;
 
-<<<<<<< HEAD
 	if (lo->lo_queue->limits.logical_block_size == arg)
 		return 0;
 
@@ -1391,18 +1390,6 @@ static int loop_set_block_size(struct loop_device *lo, unsigned long arg)
 
 	/* invalidate_bdev should have truncated all the pages */
 	if (lo->lo_device->bd_inode->i_mapping->nrpages) {
-=======
-	if (lo->lo_queue->limits.logical_block_size != arg) {
-		sync_blockdev(lo->lo_device);
-		invalidate_bdev(lo->lo_device);
-	}
-
-	blk_mq_freeze_queue(lo->lo_queue);
-
-	/* invalidate_bdev should have truncated all the pages */
-	if (lo->lo_queue->limits.logical_block_size != arg &&
-			lo->lo_device->bd_inode->i_mapping->nrpages) {
->>>>>>> 01317ebd8598 (loop: replace kill_bdev with invalidate_bdev)
 		err = -EAGAIN;
 		pr_warn("%s: loop%d (%s) has still dirty pages (nrpages=%lu)\n",
 			__func__, lo->lo_number, lo->lo_file_name,
